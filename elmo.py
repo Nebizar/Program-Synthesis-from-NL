b1b34e10b887198b4de4ba237678c78a59ec0119
Nebizar <krzysztof211@gmail.com>
2020-12-30 13:27:56 +0100

    functions added

--- elmo.py
+++ elmo.py
@@ -17,58 +17,78 @@ import plotly.graph_objs as go
 import warnings
 warnings.filterwarnings("ignore")
 
-elmo = hub.Module("https://tfhub.dev/google/elmo/3", trainable=True)
+global elmo
+global embeddings
 
-text = 'given strings var0, var1 . if length of var0 not equals to the length of var1 , return "NO". if both var0 and var1 contain at least one character "1" or both of them do not contain "1" at all , return "YES"; else return "NO" .'
+def init():
+  global elmo
+  elmo = hub.Module("https://tfhub.dev/google/elmo/3", trainable=True)
 
-sentences_dirty = text.split('.')
-
-max_len = 0
-
-sentences = []
-for sent in sentences_dirty:
-    sent = re.sub('[,;-]', '', sent)
-    if len(sent) > 0:
-        sentences.append(sent)
-        splitted = sent.split()
-        if len(splitted) > max_len:
-            max_len = len(splitted)
-
-words = []
-mask = []
-masked_words = []
-for sent in sentences:
-  splitted = sent.split()
-  #print(len(splitted))
-  for i in range(max_len):
-    try:
-      words.append(splitted[i])
-    except:
-      words.append('_')
-
-for word in words:
-  if word == "_":
-    mask.append(False)
-  else:
-    mask.append(True)
-    masked_words.append(word)
-
-print('#########################')
-print(words)
-print('#########################')
-
-embeddings = elmo(
+  global embeddings
+  embeddings = elmo(
     sentences,
     signature="default",
     as_dict=True)["elmo"]
 
 
-with tf.compat.v1.Session() as sess:
-  sess.run(tf.compat.v1.global_variables_initializer())
-  sess.run(tf.compat.v1.tables_initializer())
-  x = sess.run(embeddings)
-embs = x.reshape(-1, 1024)
-masked_embs = embs[mask]
+def get_words(text):
+  sentences_dirty = text.split('.')
+
+  max_len = 0
+
+  sentences = []
+  for sent in sentences_dirty:
+      sent = re.sub('[,;-]', '', sent)
+      if len(sent) > 0:
+          sentences.append(sent)
+          splitted = sent.split()
+          if len(splitted) > max_len:
+              max_len = len(splitted)
+
+  words = []
+  mask = []
+  masked_words = []
+  for sent in sentences:
+    splitted = sent.split()
+    for i in range(max_len):
+      try:
+        words.append(splitted[i])
+      except:
+        words.append('_')
+
+  for word in words:
+    if word == "_":
+      mask.append(False)
+    else:
+      mask.append(True)
+      masked_words.append(word)
+
+  print('#########################')
+  print(words)
+  print('#########################')
+
+  return sentences, mask
+
+def get_embeddings(text):
+  sentences, mask = get_words(text)
+
+
+
+
+  with tf.compat.v1.Session() as sess:
+    sess.run(tf.compat.v1.global_variables_initializer())
+    sess.run(tf.compat.v1.tables_initializer())
+    x = sess.run(embeddings)
+  embs = x.reshape(-1, 1024)
+  masked_embs = embs[mask]
+
+  return masked_embs
+
+
+
+text = 'given strings var0, var1 . if length of var0 not equals to the length of var1 , return "NO". if both var0 and var1 contain at least one character "1" or both of them do not contain "1" at all , return "YES"; else return "NO" .'
+
+masked_embs = get_embeddings(text)
 
 print('#########################')
 print(len(masked_embs))
